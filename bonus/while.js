f5d91956ee8d875d45be07f44c994baa7f3107da
giuseppe <Giuseppearimatea10@gmail.com>
2020-01-09 16:49:23 +0100

    commit

---
+++ bonus/while.js
@@ -0,0 +1,15 @@
+// var i = 1;
+// while( i < 101) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     document.getElementById('numeri').innerHTML += '<li>' + i + 'FizzBuzz' + '</li>';
+//   }
+//   else if (i % 3 == 0) {
+//     document.getElementById('numeri').innerHTML += '<li>' + i + 'Fizz' + '</li>';
+//   }
+//   else if (i % 5 == 0) {
+//     document.getElementById('numeri').innerHTML += '<li>' + i + 'Buzz' + '</li>';
+//   } else {
+//     document.getElementById('numeri').innerHTML += '<li>' + i + '</li>';
+//   }
+//   i++
+// }
